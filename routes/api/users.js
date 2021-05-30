074057cf20efec1899c9273678b59a6ff62701bb
Renos Efstathiou <it164826@it.teithe.gr>
2021-05-30 23:04:24 +0300

    Created user field validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 /**
  * @route  Post api/users
  * @desc   Create a user
  * @access PUBLIC
  */
-router.post('/', (req, res) => {
-  res.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').trim().not().isEmpty(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+    check('email', 'Please enter a valid email address')
+      .normalizeEmail()
+      .isEmail(),
+    check(
+      'confirm_password',
+      'Please enter a password confirmation with 6 or more characters'
+    )
+      .isLength({ min: 6 })
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          // trow error if passwords do not match
+          throw new Error("Passwords don't match");
+        } else {
+          return value;
+        }
+      })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.status('201').send(req.body);
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to the DB
 connectDB();
 
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('API running');
 });
